5503e35f665495ed1c7b9ad3df05c82665ce4fa7
GGDRHED <84715948+GGDRHED@users.noreply.github.com>
2023-06-30 16:30:54 +0300

    Update 5.2.py

--- 5.2.py
+++ 5.2.py
@@ -1,7 +1,10 @@
-codes = set()  
+codes = set()
 activat_code = input("Введите код активации: ")
-if activat_code in codes: 
-    print("Данный код уже был использован")
+if len(activat_code) != 10:
+    print("Неправильный формат кода активации")
 else:
-    codes.add(activat_code)  
-    print("Продукт активирован")
+    if activat_code in codes:
+        print("Данный код уже был использован")
+    else:
+        codes.add(activat_code)
+        print("Продукт активирован")
