edfb9cb2bdcaf7079b1b5f84fa1d683cf57826f3
GGDRHED <84715948+GGDRHED@users.noreply.github.com>
2023-07-03 15:50:34 +0300

    Add files via upload

---
+++ 6.2.py
@@ -0,0 +1,11 @@
+def get_season_by_month_number(month_number):
+    if month_number in [1, 2, 12]:
+        return 'Зима'
+    elif month_number in [3, 4, 5]:
+        return 'Весна'
+    elif month_number in [6, 7, 8]:
+        return 'Лето'
+    elif month_number in [9, 10, 11]:
+        return 'Осень'
+    else:
+        return 'Некорректный номер месяца'
